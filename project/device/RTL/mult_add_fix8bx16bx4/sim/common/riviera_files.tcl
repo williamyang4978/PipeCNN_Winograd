b455a41f899b9bba8d8d704ba1d61551ee9c119d
williamyang <57379044+williamyang4978@users.noreply.github.com>
2019-12-05 15:58:20 +0800

    Add files via upload

---
+++ project/device/RTL/mult_add_fix8bx16bx4/sim/common/riviera_files.tcl
@@ -0,0 +1,66 @@
+
+namespace eval mult_add_fix8bx16bx4 {
+  proc get_design_libraries {} {
+    set libraries [dict create]
+    dict set libraries altera_mult_add_180  1
+    dict set libraries mult_add_fix8bx16bx4 1
+    return $libraries
+  }
+  
+  proc get_memory_files {QSYS_SIMDIR} {
+    set memory_files [list]
+    return $memory_files
+  }
+  
+  proc get_common_design_files {USER_DEFINED_COMPILE_OPTIONS USER_DEFINED_VERILOG_COMPILE_OPTIONS USER_DEFINED_VHDL_COMPILE_OPTIONS QSYS_SIMDIR} {
+    set design_files [dict create]
+    return $design_files
+  }
+  
+  proc get_design_files {USER_DEFINED_COMPILE_OPTIONS USER_DEFINED_VERILOG_COMPILE_OPTIONS USER_DEFINED_VHDL_COMPILE_OPTIONS QSYS_SIMDIR} {
+    set design_files [list]
+    lappend design_files "vlog -v2k5 $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS  \"[normalize_path "$QSYS_SIMDIR/../altera_mult_add_180/sim/mult_add_fix8bx16bx4_altera_mult_add_180_4l2tjzi.v"]\"  -work altera_mult_add_180"
+    lappend design_files "vlog -v2k5 $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS  \"[normalize_path "$QSYS_SIMDIR/mult_add_fix8bx16bx4.v"]\"  -work mult_add_fix8bx16bx4"                                                      
+    return $design_files
+  }
+  
+  proc get_elab_options {SIMULATOR_TOOL_BITNESS} {
+    set ELAB_OPTIONS ""
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $ELAB_OPTIONS
+  }
+  
+  
+  proc get_sim_options {SIMULATOR_TOOL_BITNESS} {
+    set SIM_OPTIONS ""
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $SIM_OPTIONS
+  }
+  
+  
+  proc get_env_variables {SIMULATOR_TOOL_BITNESS} {
+    set ENV_VARIABLES [dict create]
+    set LD_LIBRARY_PATH [dict create]
+    dict set ENV_VARIABLES "LD_LIBRARY_PATH" $LD_LIBRARY_PATH
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $ENV_VARIABLES
+  }
+  
+  
+  proc normalize_path {FILEPATH} {
+      if {[catch { package require fileutil } err]} { 
+          return $FILEPATH 
+      } 
+      set path [fileutil::lexnormalize [file join [pwd] $FILEPATH]]  
+      if {[file pathtype $FILEPATH] eq "relative"} { 
+          set path [fileutil::relative [pwd] $path] 
+      } 
+      return $path 
+  } 
+}
---
+++ project/device/RTL/mult_add_fix8bx16bx4/sim/common/vcs_files.tcl
@@ -0,0 +1,49 @@
+
+namespace eval mult_add_fix8bx16bx4 {
+  proc get_memory_files {QSYS_SIMDIR} {
+    set memory_files [list]
+    return $memory_files
+  }
+  
+  proc get_common_design_files {QSYS_SIMDIR} {
+    set design_files [dict create]
+    return $design_files
+  }
+  
+  proc get_design_files {QSYS_SIMDIR} {
+    set design_files [dict create]
+    dict set design_files "mult_add_fix8bx16bx4_altera_mult_add_180_4l2tjzi.v" "$QSYS_SIMDIR/../altera_mult_add_180/sim/mult_add_fix8bx16bx4_altera_mult_add_180_4l2tjzi.v"
+    dict set design_files "mult_add_fix8bx16bx4.v"                             "$QSYS_SIMDIR/mult_add_fix8bx16bx4.v"                                                       
+    return $design_files
+  }
+  
+  proc get_elab_options {SIMULATOR_TOOL_BITNESS} {
+    set ELAB_OPTIONS ""
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $ELAB_OPTIONS
+  }
+  
+  
+  proc get_sim_options {SIMULATOR_TOOL_BITNESS} {
+    set SIM_OPTIONS ""
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $SIM_OPTIONS
+  }
+  
+  
+  proc get_env_variables {SIMULATOR_TOOL_BITNESS} {
+    set ENV_VARIABLES [dict create]
+    set LD_LIBRARY_PATH [dict create]
+    dict set ENV_VARIABLES "LD_LIBRARY_PATH" $LD_LIBRARY_PATH
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $ENV_VARIABLES
+  }
+  
+  
+}
